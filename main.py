8db06e7ac6c04a033772fa9007978e021cc7af0e
Yande-ol <yaancarlos8@icloud.com>
2026-08-08 21:18:15 +0100

    test: Add test_easy.map map and temporary logic in main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+from pathlib import Path
+
+src_path = Path(__file__).parent / "src"
+sys.path.append(str(src_path))
+
+from parser import MapParseError, Parser  # Sem o '.py'
+
+
+def main() -> None:
+    args: list[str] = sys.argv[1:]
+
+    if not args:
+        print("Uso: python3 main.py <caminho_do_mapa>")
+        sys.exit(1)
+
+    map_path: str = args[0]
+    parser = Parser()
+
+    try:
+        graph, nb_drones = parser.parse_file(map_path)
+        print("=== Mapa Carregado com Sucesso ===")
+        print(f"Número de drones: {nb_drones}")
+        print(f"Start Hub: {graph.start_hub}")
+        print(f"End Hub: {graph.end_hub}")
+        print(f"Total de zonas: {len(graph.zones)}")
+        print(f"Total de conexões: {len(graph.connections)}")
+    except MapParseError as err:
+        print(f"\033[91m[Erro de Parsing]\033[0m {err}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"\033[91m[Erro]\033[0m Arquivo não encontrado: {map_path}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
