4e0b9ff4497450c03b9f191f1a43bee0ec7fef30
Yande-ol <yaancarlos8@icloud.com>
2026-08-22 14:25:14 +0100

    test: Creation of a temporary logic in main.py to test pathfinder.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 from src.parser import MapParseError, Parser
+from src.pathfinder import Pathfinder
 
 src_path = Path(__file__).parent / "src"
 sys.path.append(str(src_path))
@@ -17,6 +18,7 @@ def main() -> None:
     parser = Parser()
 
     try:
+        # teste do nosso parser
         graph, nb_drones = parser.parse_file(map_path)
         print("=== Mapa Carregado com Sucesso ===")
         print(f"Número de drones: {nb_drones}")
@@ -24,6 +26,24 @@ def main() -> None:
         print(f"End Hub: {graph.end_hub}")
         print(f"Total de zonas: {len(graph.zones)}")
         print(f"Total de conexões: {len(graph.connections)}")
+
+        # teste do nosso pathfinderr
+        print("=== Tete do nosso pathfinder ===")
+        pathfinder = Pathfinder(graph)
+
+        # teste dos cominhos mais curtos(dijkstra purin)
+        shortest = pathfinder.find_shortest_path()
+        print(f"Caminho mais curto obsoluto: {shortest}")
+
+        # teste dos multiplos caminhos (rotas paralelas disjuntas)
+        paths = pathfinder.find_multiple_paths()
+        if not paths:
+            print("\033[93m[Aviso]\033[0m Nenhum caminho válido até o destino!")
+        else:
+            print(f"Total de rotas disjuntas encontradas: {len(paths)}")
+            for idx, p in enumerate(paths, start=1):
+                print(f"  • Rota {idx}: {p}")
+
     except MapParseError as err:
         print(f"\033[91m[Erro de Parsing]\033[0m {err}")
         sys.exit(1)
